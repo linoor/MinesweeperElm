5493e148e0504c430ad3c7c29d4eb10d17dfeb9c
= <michal.pomaranski@gmail.com>
2016-02-23 18:57:03 +0100

    background working

--- minesweeper.elm
+++ minesweeper.elm
@@ -23,8 +23,41 @@ type alias Row   = List Field
 type alias Board = List Row
 
 type alias Game = {
-     minesCount : Int,
-     rowCount : Int,
+     minesCount  : Int,
+     rowCount    : Int,
      columnCount : Int,
-     timeLimit : Int,
+     -- time limit in seconds
+     timeLimit   : Int,
+     timeElapsed : Int
 }
+
+defaultGame : Game
+defaultGame = {
+     minesCount  = 10,
+     rowCount    = 10,
+     columnCount = 10,
+     timeLimit   = 100,
+     timeElapsed = 0
+ }
+
+-- VIEW
+
+backgroundColor : Color
+backgroundColor = rgb 51 168 224
+
+view : Game -> Element
+view game =
+     let size = 25
+         w = size * game.columnCount
+         h = size * game.rowCount
+         screenHeight = 700
+         screenWidth = 615
+     in
+     container screenWidth screenHeight middle <|
+         collage screenWidth screenHeight [
+             rect screenWidth screenHeight |> filled backgroundColor
+     ]
+
+
+main : Element
+main = view defaultGame
