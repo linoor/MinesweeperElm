3ead8136de15db3262a3a42ec07beb05d26dec3f
= <michal.pomaranski@gmail.com>
2016-02-23 18:56:48 +0100

    added imports

--- minesweeper.elm
+++ minesweeper.elm
@@ -1,3 +1,11 @@
+import Color exposing (..)
+import Graphics.Collage exposing (..)
+import Graphics.Element exposing (..)
+--import Keyboard
+--import Text
+--import Time exposing (..)
+--import Window
+
 -- MODEL
 
 type FieldState = Covered
