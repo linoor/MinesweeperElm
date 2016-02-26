666bd72dfbe38efcf296a65837fe788b3f9ab2cf
= <michal.pomaranski@gmail.com>
2016-02-26 07:27:06 +0100

    shadow working

--- minesweeper.elm
+++ minesweeper.elm
@@ -31,31 +31,62 @@ type alias Game = {
      timeElapsed : Int
 }
 
+
+
 defaultGame : Game
 defaultGame = {
      minesCount  = 10,
-     rowCount    = 10,
-     columnCount = 10,
+     rowCount    = 20,
+     columnCount = 20,
      timeLimit   = 100,
      timeElapsed = 0
  }
 
 -- VIEW
 
-backgroundColor : Color
-backgroundColor = rgb 51 168 224
+background : Color
+background = rgb 51 168 224
+
+shadow : Color
+shadow = rgb 41 139 203
 
 view : Game -> Element
 view game =
-     let size = 25
+     let
+         size = 25
          w = size * game.columnCount
          h = size * game.rowCount
          screenHeight = 700
          screenWidth = 615
+
+         boardLength : Int
+         boardLength = size * game.columnCount
+         boardHeight : Int
+         boardHeight = size * game.rowCount
+         spaceBetween = (screenWidth-boardLength) // 2
+         spaceUnderBoard = 30
+
+         clockIconPositionX = -boardLength // 2 |> toFloat
+         clockIconPositionY = (boardHeight//2) + 20 |> toFloat
+
+         clockIconPosition : (Float, Float)
+         clockIconPosition = (clockIconPositionX, clockIconPositionY)
      in
      container screenWidth screenHeight middle <|
          collage screenWidth screenHeight [
-             rect screenWidth screenHeight |> filled backgroundColor
+             rect screenWidth screenHeight |> filled background,
+             image 128 32 "ikonki/saper_logo.png" |> toForm
+                                                  |> move (0, screenHeight/2 - (32/2) - 10),
+             image 20 20 "ikonki/zegarek.png" |> toForm
+                                              |> move clockIconPosition,
+             polygon [
+                (0.0,0.0),
+                (toFloat boardLength, 0.0),
+                (toFloat boardLength, toFloat boardHeight),
+                (toFloat (boardLength + spaceBetween), toFloat (boardHeight - size)),
+                (toFloat (boardLength + spaceBetween), -spaceUnderBoard),
+                (0.0+size, -spaceUnderBoard)
+             ] |> filled shadow |> move (-(toFloat boardLength / 2), -(screenHeight/2) + spaceUnderBoard)
      ]
 
 
